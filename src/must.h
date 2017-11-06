1734657e325795efede8637875650945a5b7d2bc
Kevin Burk <xavierholt@gmail.com>
2017-11-05 18:53:29 -0800

    Exception spec and macro fixes.

--- src/must.h
+++ src/must.h
@@ -4,10 +4,10 @@
 #include <sstream>
 #include <string>
 
-#define must_equal(a, b)     Moka::must_equal(__FILE__, __LINE__, a, b)
-#define must_not_equal(a, b) Moka::must_not_equal(__FILE__, __LINE__, a, b)
-#define must_throw(T, f)     Moka::must_throw<T>(__FILE__, __LINE__, #T, f)
-#define must_fail(m)         Moka::must_fail(__FILE__, __LINE__, m)
+#define must_equal(a, b)     Moka::must::equal(__FILE__, __LINE__, a, b)
+#define must_not_equal(a, b) Moka::must::not_equal(__FILE__, __LINE__, a, b)
+#define must_throw(T, f)     Moka::must::throoow<T>(__FILE__, __LINE__, #T, f)
+#define must_fail(m)         Moka::must::fail(__FILE__, __LINE__, m)
 
 #define R(x) "\e[31m" << (x) << "\e[0m"
 #define G(x) "\e[32m" << (x) << "\e[0m"
@@ -27,6 +27,7 @@ namespace Moka
   public:
     Failure(const std::string& m): mFile(nullptr), mLine(0), mWhat(m) {}
     Failure(const char* f, int l, const std::string& m): mFile(f), mLine(l), mWhat(m) {}
+    ~Failure() throw() {}
 
     const char* file() const {
       return mFile;
@@ -36,53 +37,55 @@ namespace Moka
       return mLine;
     }
 
-    const char* what() const noexcept {
+    const char* what() const throw() {
       return mWhat.c_str();
     }
   };
 
-  template <class A, class B>
-  void must_equal(const char* f, int l, const A& a, const B& b) {
-    if(a != b) {
-      std::stringstream message;
-      message << "Expected " << G(rep(b)) << " but got " << R(rep(a));
-      throw new Violation(f, l, message.str());
+  namespace must {
+    template <class A, class B>
+    void equal(const char* f, int l, const A& a, const B& b) {
+      if(a != b) {
+        std::stringstream message;
+        message << "Expected " << G(rep(b)) << " but got " << R(rep(a));
+        throw new Violation(f, l, message.str());
+      }
     }
-  }
 
-  void must_fail(const char* f, int l,  const char* m) {
-    std::stringstream message;
-    message << R(m);
-    throw new Violation(f, l, message.str());
-  }
-
-  template <class A, class B>
-  void must_not_equal(const char* f, int l, const A& a, const B& b) {
-    if(a == b) {
+    void fail(const char* f, int l,  const char* m) {
       std::stringstream message;
-      message << "Expected anything but " << R(rep(b)) << " but got " << R(rep(a));
-      throw new Violation(f, l, message.str());
-    }
-  }
-
-  template <class E>
-  void must_throw(const char* f, int l, const char* type, std::function<void()> fn) {
-    try {
-      fn();
-    }
-    catch(E& e) {
-      // Good catch!
-      return;
-    }
-    catch(std::exception& e) {
-      std::stringstream message;
-      message << "Expected to catch a " << G(type) << " but got " << R(e.what());
+      message << R(m);
       throw new Violation(f, l, message.str());
     }
 
-    std::stringstream message;
-    message << "Expected to catch a " << G(type) << " but nothing was thrown!";
-    throw new Violation(f, l, message.str());
+    template <class A, class B>
+    void not_equal(const char* f, int l, const A& a, const B& b) {
+      if(a == b) {
+        std::stringstream message;
+        message << "Expected anything but " << R(rep(b)) << " but got " << R(rep(a));
+        throw new Violation(f, l, message.str());
+      }
+    }
+
+    template <class E>
+    void throoow(const char* f, int l, const char* type, std::function<void()> fn) {
+      try {
+        fn();
+      }
+      catch(E& e) {
+        // Good catch!
+        return;
+      }
+      catch(std::exception& e) {
+        std::stringstream message;
+        message << "Expected to catch a " << G(type) << " but got " << R(e.what());
+        throw new Violation(f, l, message.str());
+      }
+
+      std::stringstream message;
+      message << "Expected to catch a " << G(type) << " but nothing was thrown!";
+      throw new Violation(f, l, message.str());
+    }
   }
 }
 
